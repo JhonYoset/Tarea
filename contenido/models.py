c0fa53ed50f4252d77aa4f77bdf093eded7358bd
JhonYoset <jlunaq@unsa.edu.pe>
2021-06-20 20:05:44 -0500

    aumento en modelos tambien en el admin.py admin site register( admite el modelo Cotennido)

--- contenido/models.py
+++ contenido/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
-# Create your models here.
 class Contenido(models.Model):
     nombre = models.CharField(max_length=255)
-    autor = models.CharField(max_length=200)
+    autor = models.TextField()
     texto = models.TextField()
