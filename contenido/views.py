93ed5c68d6ecedac837d4d6b38dd8c4b28ddfa5f
JhonYoset <jlunaq@unsa.edu.pe>
2021-06-20 21:01:20 -0500

    cambios en Views creo objeto contenidos y contenido

--- contenido/views.py
+++ contenido/views.py
@@ -1,3 +1,12 @@
+import contenido
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Contenido
+
+def index(request):
+    contenidos = Contenido.objects.all()
+    return render(request, 'blog/index.html', {'contenidos': contenidos})
+
+def detail(request, pk):
+    contenido = Contenido.objects.get(pk=pk)
+    return render(request, 'blog/detail.html', {'contenido': contenido})
